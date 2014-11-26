ddff2c58365a4054913e926481e88ede2a86a9c1
Peteris Krumins <peteris.krumins@gmail.com>
2014-11-26 22:47:24 +0200

    dont repeat use constant a hundred times

--- rackspace-traffic.pl
+++ rackspace-traffic.pl
@@ -12,27 +12,29 @@ use strict;
 use Text::CSV;
 
 # Rackspace Invoice CSV columns
-use constant ACCOUNT_NO => 0;
-use constant BILL_NO => 1;
-use constant BILL_START_DATE => 2;
-use constant BILL_END_DATE => 3;
-use constant SERVICE_TYPE => 4;
-use constant EVENT_TYPE => 5;
-use constant EVENT_START_DATE => 6;
-use constant EVENT_END_DATE => 7;
-use constant IMPACT_TYPE => 8;
-use constant QUANTITY => 9;
-use constant UOM => 10;
-use constant RATE => 11;
-use constant AMOUNT => 12;
-use constant USAGE_RECORD_ID => 13;
-use constant DC_ID => 14;
-use constant REGION_ID => 15;
-use constant RES_ID => 16;
-use constant RES_NAME => 17;
-use constant ATTRIBUTE_1 => 18;
-use constant ATTRIBUTE_2 => 19;
-use constant ATTRIBUTE_3 => 20;
+use constant {
+    ACCOUNT_NO => 0,
+    BILL_NO => 1,
+    BILL_START_DATE => 2,
+    BILL_END_DATE => 3,
+    SERVICE_TYPE => 4,
+    EVENT_TYPE => 5,
+    EVENT_START_DATE => 6,
+    EVENT_END_DATE => 7,
+    IMPACT_TYPE => 8,
+    QUANTITY => 9,
+    UOM => 10,
+    RATE => 11,
+    AMOUNT => 12,
+    USAGE_RECORD_ID => 13,
+    DC_ID => 14,
+    REGION_ID => 15,
+    RES_ID => 16,
+    RES_NAME => 17,
+    ATTRIBUTE_1 => 18,
+    ATTRIBUTE_2 => 19,
+    ATTRIBUTE_3 => 20
+};
 
 my $csvFile = shift or die "usage: $0 <csv file>";
 my $csv = Text::CSV->new;
